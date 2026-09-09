d31bff3277d38ab8e38b2a48221031fb28d5eb10
Kaelian Baudelet <aconique@gmail.com>
2026-09-09 08:06:28 +0200

    chore: Update seo description in layout.tsx
    Update seo description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,7 @@ export const metadata: Metadata = {
 	manifest: "/site.webmanifest",
 	openGraph: {
 		title: "Linksign - Lightweight TypeScript Wrapper for Edusign",
-		description:
-			"Interact programmatically with Edusign.",
+		description: "Interact programmatically with Edusign.",
 		url: "https://linksign.studentsphere.app",
 		siteName: "Linksign",
 		images: [
@@ -50,8 +49,7 @@ export const metadata: Metadata = {
 	twitter: {
 		card: "summary_large_image",
 		title: "Linksign - Lightweight TypeScript Wrapper for Edusign",
-		description:
-			"Interact programmatically with Edusign",
+		description: "Interact programmatically with Edusign.",
 		images: ["/banner.png"],
 	},
 };
